cdfc666eca5b9c0ec6557ef0e400bfb2c052e78e
hafenr <robin.hafen@gmail.com>
2018-11-08 22:21:31 +0100

    xmonad: remove borders in full screen

--- _xmonad/xmonad.hs
+++ _xmonad/xmonad.hs
@@ -1,15 +1,31 @@
 import XMonad
-import XMonad.Config.Xfce
+import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout
+import XMonad.Layout.NoBorders
 
 myManageHook = composeAll
     [ className =? "Do"                 --> doIgnore
     , className =? "Download"           --> doFloat
     ]
 
-main = xmonad xfceConfig
-            { modMask = mod4Mask
-            , borderWidth = 3
-            , terminal = "xfce4-terminal"
-            , manageHook = manageHook xfceConfig <+> myManageHook
-            }
+myLayoutHook = tiled ||| Mirror tiled ||| noBorders Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
+myConfig = desktopConfig
+       { modMask = mod4Mask
+       , borderWidth = 3
+       , terminal = "xfce4-terminal"
+       , manageHook = manageHook desktopConfig <+> myManageHook
+       , layoutHook = desktopLayoutModifiers $ myLayoutHook
+       }
+
+main = xmonad myConfig
